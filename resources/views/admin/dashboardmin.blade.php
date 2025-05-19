b0b38408e21c4f2ea79f5ee4cee2156d0a03f72f
Suci Ramadhani <scxquee@gmail.com>
2025-05-19 10:53:50 +0700

    meengkapi dashboard

--- resources/views/admin/dashboardmin.blade.php
+++ resources/views/admin/dashboardmin.blade.php
@@ -84,6 +84,20 @@
         <span>Management Pengguna</span>
     </a>
 </li>
+<li
+    class="sidebar-item">
+    <a href="{{ url('/datasiswa') }}" class='sidebar-link'>
+        <i class="bi bi-journal-check"></i>
+        <span>Data Siswa</span>
+    </a>
+</li>
+<li
+    class="sidebar-item">
+    <a href="{{ url('/dataperusahaan') }}" class='sidebar-link'>
+        <i class="bi bi-journal-check"></i>
+        <span>Data Perusahaan</span>
+    </a>
+</li>
     <li
     class="sidebar-item">
     <a href="{{ url('/notif') }}" class='sidebar-link'>
@@ -197,40 +211,6 @@
                                     <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
                                         <h6 class="text-muted font-semibold">Perusahaan Partner</h6>
                                         <h6 class="font-extrabold mb-0">100</h6>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-6 col-lg-3 col-md-6">
-                        <div class="card">
-                            <div class="card-body px-4 py-4-5">
-                                <div class="row">
-                                    <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
-                                        <div class="stats-icon green mb-2">
-                                            <i class="iconly-boldGraph"></i>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                        <h6 class="text-muted font-semibold">Server Uptime</h6>
-                                        <h6 class="font-extrabold mb-0">95.2%</h6>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-6 col-lg-3 col-md-6">
-                        <div class="card">
-                            <div class="card-body px-4 py-4-5">
-                                <div class="row">
-                                    <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
-                                        <div class="stats-icon red mb-2">
-                                            <i class="iconly-boldBookmark"></i>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                        <h6 class="text-muted font-semibold">Kehadiran Hari Ini</h6>
-                                        <h6 class="font-extrabold mb-0">250</h6>
                                     </div>
                                 </div>
                             </div>
